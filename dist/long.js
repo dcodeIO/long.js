bcebd51e9708060a27c7d6de443da0647c4f7697
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-02-02 14:45:47 -0800

    remainder too

--- dist/long.js
+++ dist/long.js
@@ -39,7 +39,7 @@
 
     try {
       wasm = new WebAssembly.Instance(new WebAssembly.Module(new Uint8Array([
-        0, 97, 115, 109, 1, 0, 0, 0, 1, 13, 2, 96, 0, 1, 127, 96, 4, 127, 127, 127, 127, 1, 127, 3, 5, 4, 0, 1, 1, 1, 6, 6, 1, 127, 1, 65, 0, 11, 7, 34, 4, 3, 109, 117, 108, 0, 1, 5, 100, 105, 118, 95, 115, 0, 2, 5, 100, 105, 118, 95, 117, 0, 3, 8, 103, 101, 116, 95, 104, 105, 103, 104, 0, 0, 10, 117, 4, 4, 0, 35, 0, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 126, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 127, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 128, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11
+        0, 97, 115, 109, 1, 0, 0, 0, 1, 13, 2, 96, 0, 1, 127, 96, 4, 127, 127, 127, 127, 1, 127, 3, 7, 6, 0, 1, 1, 1, 1, 1, 6, 6, 1, 127, 1, 65, 0, 11, 7, 50, 6, 3, 109, 117, 108, 0, 1, 5, 100, 105, 118, 95, 115, 0, 2, 5, 100, 105, 118, 95, 117, 0, 3, 5, 114, 101, 109, 95, 115, 0, 4, 5, 114, 101, 109, 95, 117, 0, 5, 8, 103, 101, 116, 95, 104, 105, 103, 104, 0, 0, 10, 191, 1, 6, 4, 0, 35, 0, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 126, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 127, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 128, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 129, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 130, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11
       ])), {}).exports;
     } catch (e) {
       // no wasm support :(
@@ -851,11 +851,11 @@
 
         // use wasm support if present
         if (wasm) {
-          var low = wasm.mul(this.low,
-                             this.high,
-                             multiplier.low,
-                             multiplier.high);
-          return fromBits(low, wasm.get_high(), this.unsigned);
+            var low = wasm.mul(this.low,
+                               this.high,
+                               multiplier.low,
+                               multiplier.high);
+            return fromBits(low, wasm.get_high(), this.unsigned);
         }
 
         if (multiplier.isZero())
@@ -934,13 +934,13 @@
 
         // use wasm support if present
         if (wasm) {
-          var low = (this.unsigned ? wasm.div_u : wasm.div_s)(
-            this.low,
-            this.high,
-            divisor.low,
-            divisor.high
-          );
-          return fromBits(low, wasm.get_high(), this.unsigned);
+            var low = (this.unsigned ? wasm.div_u : wasm.div_s)(
+                this.low,
+                this.high,
+                divisor.low,
+                divisor.high
+            );
+            return fromBits(low, wasm.get_high(), this.unsigned);
         }
 
         if (divisor.isZero())
@@ -1042,6 +1042,18 @@
     LongPrototype.modulo = function modulo(divisor) {
         if (!isLong(divisor))
             divisor = fromValue(divisor);
+
+        // use wasm support if present
+        if (wasm) {
+            var low = (this.unsigned ? wasm.rem_u : wasm.rem_s)(
+                this.low,
+                this.high,
+                divisor.low,
+                divisor.high
+            );
+            return fromBits(low, wasm.get_high(), this.unsigned);
+        }
+
         return this.sub(this.div(divisor).mul(divisor));
     };
 
