9410cc4361b08c40e73c779fcf215e696caeae62
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-07-18 20:10:21 +0200

    feat: make it ESM

--- src/long.js
+++ src/long.js
@@ -23,7 +23,7 @@ try {
  * @param {boolean=} unsigned Whether unsigned or not, defaults to signed
  * @constructor
  */
-function Long(low, high, unsigned) {
+export default function Long(low, high, unsigned) {
 
     /**
      * The low 32 bits as a signed value.
