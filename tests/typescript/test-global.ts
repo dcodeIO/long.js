516961cf75857c93c2981b5e0d950ebb276cb057
dcode <dcode@dcode.io>
2025-02-13 10:52:47 +0100

    Update UMD fallback

--- tests/typescript/test-global.ts
+++ tests/typescript/test-global.ts
@@ -1,7 +1,5 @@
-import Long from "../../index.js";
+/// <reference path="../../umd/index.d.ts" />
 
 Long.fromValue(1);
 var long = new Long(0, 0);
 long.toNumber();
-
-export default Long;
--- tests/typescript/test-require.ts
+++ tests/typescript/test-require.ts
@@ -1,5 +1,7 @@
 import Long = require("../../umd/index.js");
 
 Long.fromValue(1);
+var long = new Long(0, 0);
+long.toNumber();
 
 export default Long;
